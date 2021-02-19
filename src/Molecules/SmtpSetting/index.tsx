104bb4328c7a636f65d958b69f17dcfd48c9ad77
satria_herman <satriaherman967@gmail.com>
2021-02-19 18:17:05 +0700

    change width in card

--- src/Molecules/SmtpSetting/index.tsx
+++ src/Molecules/SmtpSetting/index.tsx
@@ -41,14 +41,14 @@ export class SmtpSetting extends React.Component<SmtpSettingProps, State> {
     return (
       
        <div className="col-md-6 mt-4" key={this.props.key} style={{ marginRight: '0 !important' }}>
-        <div className="card mx-auto" style={{ width:'90' }}>
+        <div className="card mx-auto" style={{ width:'90%' }}>
             <div className="card-body">
               <Input name="host" label="Host" value={this.state.host} onChange={(e) => this.onChange(e)} />
               <Input name="port" label="Port" value={this.state.port} onChange={(e) => this.onChange(e)} />
               <Input name="user" label="User" value={this.state.user} onChange={(e) => this.onChange(e)} />
               <Input name="password" label="Password" value={this.state.password} onChange={(e) => this.onChange(e)} />
               <Checkbox name="secure" label="secure" idForCheckBox="secure" onChange={(e) => this.onChange(e)} />
-              <Button button="remove" />
+              <Button button="remove" style={{ background : '#fff !important' }} />
             </div>
           </div>
        </div>
