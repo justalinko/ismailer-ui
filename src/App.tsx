35adf58d28b194902a18165386ee828359c0d65d
RizkyArifNur <rizkyarifnur@gmail.com>
2021-02-21 23:12:21 +0700

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,202 @@
 import React from "react";
-import "./App.css";
-import "./Styles/w3.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import clsx from "clsx";
+import { createStyles, makeStyles, useTheme, Theme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import SettingIcon from "@material-ui/icons/Settings";
+import InfoIcon from "@material-ui/icons/Info";
+import SendIcon from "@material-ui/icons/Send";
+import MailIcon from "@material-ui/icons/Mail";
+import PolicyIcon from "@material-ui/icons/Policy";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { SenderSettingPage } from "./Page/SenderSetting";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { SmtpSettingPage } from "./Page/SmtpSetting";
-import { AboutPage } from "./Page/About";
 import { LicensePage } from "./Page/License";
+import { AboutPage } from "./Page/About";
 
-function App() {
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    hide: {
+      display: "none",
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: "nowrap",
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerClose: {
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: "hidden",
+      width: theme.spacing(7) + 1,
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9) + 1,
+      },
+    },
+    toolbar: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  })
+);
+
+export default function MiniDrawer() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <Router>
-        <div className="w3-sidebar w3-light-grey w3-bar-block" style={{ width: "25%" }}>
-          <h3 className="w3-bar-item">isMailer v1.0</h3>
-          <Link to="/smtp" className="w3-bar-item w3-button">
-            SMTP Settings
-          </Link>
-          <Link to="/sender" className="w3-bar-item w3-button">
-            Sender Settings
-          </Link>
-          <Link to="/about" className="w3-bar-item w3-button">
-            About
-          </Link>
-          <Link to="/license" className="w3-bar-item w3-button">
-            License
-          </Link>
-        </div>
-        <Switch>
-          <Route exact path="/sender" component={SenderSettingPage} />
-          <Route exact path="/smtp" component={SmtpSettingPage} />
-          <Route exact path="/about" component={AboutPage} />
-          <Route exact path="/license" component={LicensePage} />
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              Ismailer Client
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
+        >
+          <div className={classes.toolbar}>
+            Testing
+            <IconButton onClick={handleDrawerClose}>{theme.direction === "rtl" ? <ChevronRightIcon /> : <ChevronLeftIcon />}</IconButton>
+          </div>
+          <Divider />
+          <List>
+            <ListItem button>
+              <Link to="/smtp">
+                <ListItemIcon>
+                  <SettingIcon />
+                </ListItemIcon>
+                <ListItemText primary={"SMTP Setting"} />
+              </Link>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Sender Setting"} />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <SendIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Mail Sending"} />
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
+              <ListItemText primary={"About"} />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <PolicyIcon />
+              </ListItemIcon>
+              <ListItemText primary={"License"} />
+            </ListItem>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <Switch>
+            <Route exact path="/sender" component={SenderSettingPage} />
+            <Route exact path="/smtp" component={SmtpSettingPage} />
+            <Route exact path="/about" component={AboutPage} />
+            <Route exact path="/license" component={LicensePage} />
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
-
-export default App;
